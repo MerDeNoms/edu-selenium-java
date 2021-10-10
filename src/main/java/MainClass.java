ecadba92cf1025f563c8fb0cc3fa1cf9d1efa9bd
MerDeNoms <bjorn.alex@yandex.ru>
2021-10-10 23:20:41 +0300

    Lesson 41 is done

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,7 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -10,21 +12,20 @@ public class MainClass {
         System.setProperty("webserver.chrome.driver", "D://QA_Auto/chromedriver_win32/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
 
-        driver.manage().window().setSize(new Dimension(900, 500));
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        WebElement link = driver.findElement(By.linkText("Log in"));
+        WebElement link2 = driver.findElement(By.partialLinkText("Donate"));
+        WebElement searchField = driver.findElement(By.name("search"));
+        WebElement searchButton = driver.findElement(By.className("searchButton"));
+        WebElement li = driver.findElement(By.id("ca-viewsource"));
+        WebElement input = driver.findElement(By.tagName("input"));
+        WebElement element = driver.findElement(By.cssSelector("div#simpleSearch input#searchButton"));
+        WebElement logo = driver.findElement(By.xpath("//div[@id='mw-panel']/div[@id='p-logo']//a"));
 
-        driver.get("https://www.google.com");
-        driver.navigate().to("https://www.selenium.dev");
-        driver.navigate().back();
-        driver.navigate().forward();
-        driver.navigate().refresh();
-
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
-
-        driver.quit();
+//        driver.quit();
 
     }
 }
