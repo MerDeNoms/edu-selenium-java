0379b9b17d7be86ba3b617a252c5c8a79584d32a
MerDeNoms <bjorn.alex@yandex.ru>
2021-10-10 22:46:35 +0300

    Lesson 39 is done

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -6,7 +7,19 @@ public class MainClass {
         System.setProperty("webserver.chrome.driver", "D://QA_Auto/chromedriver_win32/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev");
+
+        driver.manage().window().setSize(new Dimension(900, 500));
+
+        driver.get("https://www.google.com");
+        driver.navigate().to("https://www.selenium.dev");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        driver.quit();
 
     }
 }
