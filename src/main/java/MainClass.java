4b9406704a1c050df0d79608ebca9be06c3916d3
MerDeNoms <bjorn.alex@yandex.ru>
2021-10-13 13:27:17 +0300

    Lesson 43 is done

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -9,26 +9,24 @@ import java.time.Duration;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.setProperty("webserver.chrome.driver", "D://QA_Auto/chromedriver_win32/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\kuzmin_ad\\QA_Auto\\chromedriver_win32\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
 
         driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        driver.findElement(By.xpath("//input[@type='search']")).sendKeys("Selenium Webdriver");
         driver.findElement(By.xpath("//input[@id='searchButton']")).click();
+        System.out.println(driver.findElement(By.xpath("//div[@id='searchText']/input")).getAttribute("value"));
+        driver.findElement(By.xpath("//div[@id='searchText']/input")).clear();
 
-        driver.get("https://github.com");
-        WebElement button = driver.findElement(By.xpath("//button[@type='submit']"));
-        if (button.getText().equals("Sign up for GitHub")) {
-            System.out.println("Success");
-        } else System.out.println("Fail");
-//        button.submit();
-
-        driver.findElement(By.xpath("//a[contains(text(),'Sign in')]")).click();
 
         driver.get("http://en-gb.facebook.com");
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("testmail");
+        driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("testpass");
         driver.findElement(By.xpath("//button[@name='login']")).submit();
+
 
 //        driver.quit();
 
